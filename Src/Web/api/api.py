e3e2e28f37631c0bd059d4b8374812b90875f7e5
Jacob <uhe1231@163.com>
2019-01-27 11:42:55 +0800

    [clean] delete proxy stat and proxy token

--- Src/Web/api/api.py
+++ Src/Web/api/api.py
@@ -48,10 +48,6 @@ API_LIST = {
             }
         },
         "desc": "Get All Proxy",
-    },
-    "/api/v1/proxies/stat/": {
-        "args": {},
-        "desc": "Statistics All Vaild Proxies",
     }
 }
 
@@ -80,15 +76,11 @@ class Proxy(Resource):
 
         options = {
             "https": bool(self.args.get('https')),
-            "token": self.args.get('token'),
             "proxy_type": self.args.get('type'),
             "proxy_region": self.args.get('region'),
         }
 
-        if options.get("token", None):
-            data = proxy_manager.getQualityProxy(**options)
-        else:
-            data = proxy_manager.getSampleProxy(**options)
+        data = proxy_manager.getSampleProxy(**options)
         
         if data:
             del data["_id"]
@@ -131,45 +123,6 @@ class Proxies(Resource):
 
         return result
 
-class ProxyCounter(dict):
-     def __missing__(self, key):
-        result = 0
-        if key in ["region_list", "https", "proxy_type", "last_status", "available_rate"]:
-            result = ProxyCounter()
-            self[key] = result
-
-        return result
-
-
-def stat(data):
-    result = ProxyCounter()
-
-    for item in data:
-        for k, v in item.items():
-            if k == "region_list":
-                for region in v:
-                    result[k][region] = result[k][region] + 1
-            elif k == "https":
-                result[k][v] = result[k][v] + 1
-            elif k == "proxy_type":
-                result[k][v] = result[k][v] + 1
-            elif k == "last_status":
-                result[k][v] = result[k][v] + 1
-            elif k == "available_rate":
-                v = int(v * 10) * 10
-                result[k][v] = result[k][v] + 1
-
-    return result
-
-class ProxiesStat(Resource):
-    def get(self):
-        result = {}
-
-        data = proxy_manager.getUsefulProxyStat()
-        result["data"] = stat(data)
-
-        return result
-
 def init_api(app):
     @app.errorhandler(404)
     def miss(e):
@@ -188,7 +141,6 @@ def init_app(app):
     init_api(app)
 
     api = Api(app)
-    api.add_resource(ProxiesStat, '/api/v1/proxies/stat/')
     api.add_resource(Proxies, '/api/v1/proxies/')
     api.add_resource(Proxy, '/api/v1/proxy/')
     api.add_resource(ApiList, '/api/v1/')
